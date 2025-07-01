a367845772d874fbf5453d3aa076498b3fcde064
moinsaleheen <moinkh786786@gmail.om>
2025-07-02 02:26:21 +0500

    Mini Hackathon

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".single-item").slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
